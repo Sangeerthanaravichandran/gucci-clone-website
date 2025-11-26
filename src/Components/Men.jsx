e27b75306a6f924ebd42a9190a7cd649cfe63117
Your Name <shreyasureshbau11@gmail.com>
2025-11-26 16:17:09 +0530

    men

--- src/Components/Men.jsx
+++ src/Components/Men.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import "../App.css";
 
 function Men() {
   return (
@@ -11,9 +12,17 @@ function Men() {
             <Link to="/Child">Child</Link>
           </nav>
           
-          <div>
-            <h1>Men page </h1>
-          </div>
+          <div className="hero-container">
+      <div className="hero-overlay">
+        <div className="hero-text">
+          <p className="new-in">NEW IN:</p>
+          <h1 className="main-title">Men</h1>
+          <p className="subtitle">
+            Shop Fall Winter 2025 men's ready-to-wear, accessories, and the latest arrivals.
+          </p>
+        </div>
+      </div>
+    </div>
         </>
   )
 }

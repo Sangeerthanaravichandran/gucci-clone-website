ecf0fa73c8e1dcb6b611c3e0b101e0c9df01e4eb
Sangeerthanaravichandran <sangeerthanaravichandran204@gmail.com>
2025-11-26 15:13:56 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Landingpage from "./Components/Landingpage";
+import Men from "./Components/Men";
+import Women from "./Components/Women";
+import Child from "./Components/Child";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landingpage />}></Route>
+          <Route path="/Men" element={<Men />}></Route>
+          <Route path="/Women" element={<Women />}></Route>
+          <Route path="/Child" element={<Child />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
-export default App
+export default App;

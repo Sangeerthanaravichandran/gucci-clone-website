a894553ad05c3a2eefdc78a43b7a352a8fb3fc15
Sangeerthanaravichandran <sangeerthanaravichandran204@gmail.com>
2025-11-26 15:53:26 +0530

    landingpage

--- src/Components/Landingpage.jsx
+++ src/Components/Landingpage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+
+
 export default function Landingpage() {
   return (
     <>
@@ -9,6 +11,21 @@ export default function Landingpage() {
         <Link to="/Women">Women</Link>
         <Link to="/Child">Child</Link>
       </nav>
+
+{/* herosection */}
+      <section className="gucci-hero">
+        <div className="hero-content">
+          <h1>Gucci Gift.</h1>
+
+          <div className="buttons">
+            <button className="btn">FOR HER</button>
+            <button className="btn">FOR HIM</button>
+          </div>
+        </div>
+      </section>
+    
+  
+
     </>
   );
 }

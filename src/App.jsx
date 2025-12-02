5939b07b9c7f9cda2d3b3feb4eb1b5e915a89a61
Sangeerthanaravichandran <sangeerthanaravichandran204@gmail.com>
2025-12-02 15:55:25 +0530

    landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,13 @@ import Men from "./Components/Men";
 import Women from "./Components/Women";
 import Child from "./Components/Child";
 import Contact from "./Components/Contact";
-<<<<<<< HEAD
 import Login from "./Components/Login";
 import ProductDetails from "./Components/productDetails";
 
-=======
 import ProductDetails from "./Components/ProductDetails";
->>>>>>> 557b7b47a6e11856190d7c7489fbf17c0e90a80e
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landingpage />}></Route>
-          <Route path="/Men" element={<Men />}></Route>
-          <Route path="/Women" element={<Women />}></Route>
-          <Route path="/Child" element={<Child />}></Route>
-          <Route path="/Contact" element={<Contact />} /> 
-          <Route path="/Login" element={< Login/>}/>
-          <Route path="/ProductDetails" element={<ProductDetails/>}/>
-        </Routes>
-      </BrowserRouter>
-     
-    </>
-=======
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landingpage />} />
@@ -41,6 +22,7 @@ function App() {
         <Route path="/men" element={<Men />} />
         <Route path="/women" element={<Women />} />
         <Route path="/child" element={<Child />} />
+        <Route path="/Login" element={< Login/>}/>
 
         {/* Product Detail Page FIXED */}
         <Route path="/product/:id" element={<ProductDetails />} />
@@ -48,7 +30,6 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
->>>>>>> 557b7b47a6e11856190d7c7489fbf17c0e90a80e
   );
 }
 
